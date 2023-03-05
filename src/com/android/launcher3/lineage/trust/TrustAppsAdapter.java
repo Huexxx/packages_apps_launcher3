d713e5085eb045b5235460b0f1476d65bcb70061
Huexxx <huexxx@gmail.com>
2023-03-05 20:25:28 +0100

    Launcher3: Use Cherish AppLock API

--- src/com/android/launcher3/lineage/trust/TrustAppsAdapter.java
+++ src/com/android/launcher3/lineage/trust/TrustAppsAdapter.java
@@ -33,7 +33,7 @@ import androidx.annotation.NonNull;
 import androidx.recyclerview.widget.DiffUtil;
 import androidx.recyclerview.widget.RecyclerView;
 
-import com.android.internal.util.evolution.EvolutionUtils;
+import com.android.internal.util.cherish.CherishUtils;
 import com.android.launcher3.R;
 import com.android.launcher3.lineage.trust.db.TrustComponent;
 
@@ -108,7 +108,7 @@ class TrustAppsAdapter extends RecyclerView.Adapter<TrustAppsAdapter.ViewHolder>
 
             mProtectedView.setVisibility(hasSecureKeyguard ? View.VISIBLE : View.GONE);
 
-            mHiddenView.setVisibility(EvolutionUtils.launchablePackages(mContext).contains(component.getPackageName()) ?
+            mHiddenView.setVisibility(CherishUtils.launchablePackages(mContext).contains(component.getPackageName()) ?
                     View.VISIBLE : View.GONE);
 
             mHiddenView.setOnClickListener(v -> {
